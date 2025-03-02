6671c477d8530e79f3d2a50c74f5e643dbe23087
Luyangt <tly9834@gmail.com>
2025-03-02 11:19:11 -0500

    revise responsive

--- src/components/projects/GrandTheftAutonomous.js
+++ src/components/projects/GrandTheftAutonomous.js
@@ -13,25 +13,29 @@ const GrandTheftAutonomous = () => {
               <h1>Autonomous Robotics Project</h1>
               <h2>"Grand Theft Autonomous" - Can-Grabbing Robot</h2>
             </div>
-            <p>
-              During the <strong>Fall 2022-2023 semester</strong>, I took the <strong>Design of Mechatronic Systems</strong> course taught by <strong>Prof. Mark Yim</strong>. This course introduced key components commonly used in mechatronic systems, including <strong>microprocessors, motors, servos, and sensors</strong>, and provided insights into the process of building integrated mechatronic systems. For the <strong>final project</strong>, we were tasked with designing <strong>a wheeled robot</strong> capable of <strong>autonomous wall following</strong> and <strong>target detection</strong>. To achieve these functionalities, we:
-            </p>
-            <ul>
-              <li>Selected ESP32 as the robot's controller</li>
-              <li>Used two motors with encoders for precise motion control</li>
-              <li>Implemented wall-following functionality using ToF and ultrasonic sensors</li>
-              <li>Powered the system with a lithium battery</li>
-            </ul>
-            <p>
-              By the end of the project, our robot successfully met all the design requirements and demonstrated stable performance in autonomous navigation and target interaction.
-            </p>
-          </div>
-          <div className="gta-header-image">
-            <img 
-              src="/images/Projects/Grand Theft Autonomous/header.png" 
-              alt="Grand Theft Autonomous"
-              className="gta-header-img"
-            />
+            <div className="gta-header-content">
+              <div className="gta-header-paragraph">
+                <p>
+                  During the <strong>Fall 2022-2023 semester</strong>, I took the <strong>Design of Mechatronic Systems</strong> course taught by <strong>Prof. Mark Yim</strong>. This course introduced key components commonly used in mechatronic systems, including <strong>microprocessors, motors, servos, and sensors</strong>, and provided insights into the process of building integrated mechatronic systems. For the <strong>final project</strong>, we were tasked with designing <strong>a wheeled robot</strong> capable of <strong>autonomous wall following</strong> and <strong>target detection</strong>. To achieve these functionalities, we:
+                </p>
+                <ul>
+                  <li>Selected ESP32 as the robot's controller</li>
+                  <li>Used two motors with encoders for precise motion control</li>
+                  <li>Implemented wall-following functionality using ToF and ultrasonic sensors</li>
+                  <li>Powered the system with a lithium battery</li>
+                </ul>
+                <p>
+                  By the end of the project, our robot successfully met all the design requirements and demonstrated stable performance in autonomous navigation and target interaction.
+                </p>
+              </div>
+              <div className="gta-header-image">
+                <img 
+                  src="/images/Projects/Grand Theft Autonomous/header.png" 
+                  alt="Grand Theft Autonomous"
+                  className="gta-header-img"
+                />
+              </div>
+            </div>
           </div>
         </div>
 
